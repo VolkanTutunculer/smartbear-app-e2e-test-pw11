41b12089ae086218c00749d2d7106ac1558f7974
volkan.tutunculer <volkan.tutunculer@gmail.com>
2025-06-09 20:45:44 -0400

    update

---
+++ tests/setup/login.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('/');
+  await page.getByRole('textbox', { name: 'Username:' }).fill('Tester');
+  await page.getByRole('textbox', { name: 'Password:' }).fill('test');
+  await page.getByRole('button', { name: 'Login' }).click();
+  
+  await expect(page.getByText('Welcome, Tester! | Logout')).toBeVisible();
+});
